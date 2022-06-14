80c9f5e1d59fc41f4039daa52aaf467d49a3ef52
leonaire <hexin0515@gmail.com>
2022-06-14 14:14:44 +0200

    corret order varFeatures

--- dump_data.py
+++ dump_data.py
@@ -30,20 +30,27 @@ def dumpRowFeatures(filename,row_features):
     file.close()
 
 def dumpVariableFeatures(filename,variable_features):
-	value = variable_features[variable_features[:,0].argsort()]
-	
-	features = {
-		"names":["objective","is_type_binary","is_type_integer","is_type_implicit_integer",\
-		         "is_type_continuous","has_lower_bound","has_upper_bound","normed_reduced_cost",\
-		         "solution_value","solution_frac","is_solution_at_lower_bound","is_solution_at_upper_bound",\
-		         "scaled_age","incumbent_value","average_incumbent_value","is_basis_lower","is_basis_basic",\
-		         "is_basis_upper","is_basis_zero"],
-		"values":value[:,:-1].tolist()
-	}
-	data = json.dumps(features)
-	file = open(filename,'w')
-	file.write(data)
-	file.close()
+    original_indice = variable_features[:,-1]
+    l = len(original_indice)
+    dict_indice = np.zeros((l))
+    for i in range(l):
+        dict_indice[int(original_indice[i])] = i
+    for i in range(l):
+        variable_features[i][-1] = dict_indice[i]
+   
+    value = variable_features[variable_features[:,-1].argsort()]
+    features = {
+        "names":["objective","is_type_binary","is_type_integer","is_type_implicit_integer",\
+        "is_type_continuous","has_lower_bound","has_upper_bound","normed_reduced_cost",\
+        "solution_value","solution_frac","is_solution_at_lower_bound","is_solution_at_upper_bound",\
+        "scaled_age","incumbent_value","average_incumbent_value","is_basis_lower","is_basis_basic",\
+        "is_basis_upper","is_basis_zero"],
+    "values":value[:,:-1].tolist()
+    }
+    data = json.dumps(features)
+    file = open(filename,'w')
+    file.write(data)
+    file.close()
 
 def dumpSolution_Ecole(filename,pyscip):
     bestsol = pyscip.getBestSol()
