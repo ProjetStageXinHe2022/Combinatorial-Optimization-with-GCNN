5e2cc2afe61c0755b8d091a84eced8771ea9bde1
Leonaire <hexin0515@gmail.com>
2022-08-12 19:14:59 +0200

    create GCN.py

--- GCN.py
+++ GCN.py
@@ -1,54 +1,48 @@
-import pickle as pkl
-import json
-import ast
+import torch
+import torch.nn as nn
 
-"""
-param:path of folder
-return :data_train_X,data_train_Y,data_validation_X,data_validation_Y,data_test_X,data_test_Y
-"""
+class simpleMLP(nn.Module):
+    def __init__(self, input_size, out_size, activate=nn.ReLU()):
+        super(simpleMLP, self).__init__()
+        self.linear = nn.Linear(input_size, out_size)
+        self.activate = activate
 
-
-class MLP(nn.Module):
-    def __init__(self, in_channel, out_channel):
-        super(MLP, self).__init__()
-        self.linear = nn.Linear(in_channel, out_channel)
-        self.activate = nn.ReLU()
-
-    def forward(self, x, A):
+    def forward(self, x):
         x = self.linear(x)
         x = self.activate(x)
-        x = A.mm(x)
         return x
 
 
-class Net(nn.Module):
-    def __init__(self, in_channel, aux_channel, out_channel):
-        super(Net, self).__init__()
-        self.layer = nn.Sequential(
-            nn.Linear(aux_channel, aux_channel),
-            nn.ReLU(),
-            nn.Linear(aux_channel, aux_channel),
+class GNN(nn.Module):
+    def __init__(self, input_size, hidden_size, nb_MLP, layerNorm=False):
+        super(GNN, self).__init__()
+        self.size_mlp = nb_MLP
+        self.mlp = nn.ModuleList(
+            [simpleMLP(input_size, hidden_size)] + [simpleMLP(hidden_size, hidden_size)] + [
+                simpleMLP(hidden_size * 2, hidden_size) for i in range(self.size_mlp - 2)]
         )
-        self.mlp = MLP(in_channel, aux_channel)
-        self.outlayer = nn.Sequential(
-            nn.Linear(aux_channel, aux_channel),
-            nn.ReLU(),
-            nn.Linear(aux_channel, out_channel),
-            #             nn.LogSoftmax()
-            nn.Sigmoid()
+        self.layerNorm = nn.ModuleList(
+            [nn.LayerNorm(hidden_size) for i in range(self.size_mlp)]
         )
 
-    def forward(self, x, A, nb_net):
-        x = self.mlp(x, A)
-        for i in range(nb_net):
-            x = self.layer(x)
-        x = self.outlayer(x)
+    def forward(self, x, A):
+        for i in range(self.size_mlp):
+            x = self.mlp[i](x)
+            y = A @ x
+            y = self.layerNorm[i](y)
+            if i != 0:
+                x = torch.cat([y, x], 1)  # sum plutot que con
+            else:
+                x = y
         return x
 
 
+class VariablePredictor(nn.Module):
+    def __init__(self, input_size, hidden_size, nb_MLP):
+        super(VariablePredictor, self).__init__()
+        self.GNN = GNN(input_size, hidden_size, nb_MLP)
+        self.outlayer = simpleMLP(hidden_size * 2, 1, nn.Sigmoid())
 
-
-
-split = [0.70,0.15,0.15]
-dataSet = load_data()
-data_train,data_validation,data_test = torch.utils.data.random_split(dataSet,[int(len(dataSet) * s) for s in split])
+    def forward(self, x, A):
+        x = self.GNN(x, A)
+        return self.outlayer(x)
